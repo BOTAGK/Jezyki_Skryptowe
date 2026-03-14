03b4c1fdb671248f812fde23b14b7c6876a09ad0
Jakub Bogacz <jakubbogacz05@gmail.com>
2026-03-15 00:20:46 +0100

    poprawa countow

--- List1/count_characters.py
+++ List1/count_characters.py
@@ -1,19 +1,23 @@
 import sys
 
-from List1.common import get_safe_char_stream
-from common import set_up_Streams, run_safely
+from common import set_up_Streams, run_safely, get_safe_char_stream
 
 #mozna dodac rzucanie wyjaktu jesli nie ma wejscia
 def countCharacters():
     set_up_Streams()
 
-    characterCounter = 0
+    totalCharacters = calculateCharactersCount()
 
-    for c in get_safe_char_stream():
-        if not c.isspace():
-            characterCounter += 1
+    print(totalCharacters)
 
-    print(characterCounter)
+def calculateCharactersCount():
+     characterCounter = 0
+
+     for c in get_safe_char_stream():
+         if not c.isspace():
+             characterCounter += 1
+
+     return characterCounter
 
 if __name__ == "__main__":
     run_safely(countCharacters)
--- List1/count_paragraphs.py
+++ List1/count_paragraphs.py
@@ -1,11 +1,16 @@
 import sys
 
-from List1.common import get_safe_char_stream
-from common import run_safely, set_up_Streams
+
+from common import run_safely, set_up_Streams, get_safe_char_stream
 
 def countParagraphs():
     set_up_Streams()
 
+    paragraphCount = calculateParagraphsCount()
+
+    print(paragraphCount)
+
+def calculateParagraphsCount():
     paragraphCount = 0
     consecutiveBlankLines = 0
     hasText = False
@@ -28,8 +33,7 @@ def countParagraphs():
     if hasText:
         paragraphCount += 1
 
-    print(paragraphCount)
-
+    return paragraphCount
 
 if __name__ == "__main__":
     run_safely(countParagraphs)
