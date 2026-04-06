0597fc2120bd98e4bd37494a4e9f6413211101fd
Jakub Bogacz <jakubbogacz05@gmail.com>
2026-04-06 17:17:10 +0200

    feat(zad4): add analyzer and manager

---
+++ List3/analyzer.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+from collections import Counter
+import sys
+
+
+def analyze_path(file_path: Path) -> dict:
+    
+    with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+        content = file.read()
+
+    lines = content.splitlines()
+    words = content.split()
+
+    char_counts = Counter(content)   
+    word_counts = Counter(words)
+
+    most_freq_char = char_counts.most_common(1)[0][0] if char_counts else ""
+    most_freq_word = word_counts.most_common(1)[0][0] if word_counts else ""
+
+    return {
+        "file_path": str(file_path),
+        "total_chars": len(content),
+        "total_words": len(words),
+        "total_lines": len(lines),
+        "most_freq_char": most_freq_char,
+        "most_freq_word": most_freq_word
+    }
+
+def main():
+    input_path = sys.stdin.read().strip()
+
+    if not input_path:
+        sys.exit(1)
+
+    file_path = Path(input_path)
+    if file_path.is_file():
+        stats = analyze_path(file_path)
+        print(json.dumps(stats))
+
+if __name__ == "__main__":
+    main()           
