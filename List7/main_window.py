fcd2549b088314802874cf614981230136856aae
Jakub Bogacz <jakubbogacz05@gmail.com>
2026-06-05 18:35:12 +0200

    chore: change the size of the window

--- List7/main_window.py
+++ List7/main_window.py
@@ -24,6 +24,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(570, 638)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.mainLayout = QVBoxLayout(self.centralwidget)
